85a19ed6c9d172e03cb492f9701fc30d2b5b46f5
Aarthi <aarthisrinkl@gmail.com>
2024-11-03 19:23:13 +0530

    sample code

--- logger_files/sample.py
+++ logger_files/sample.py
@@ -9,8 +9,8 @@ logger1.setLevel(logging.DEBUG)
 logger2.setLevel(logging.INFO)
 
 # Create file handlers for each logger
-handler1 = logging.FileHandler('log1.txt')
-handler2 = logging.FileHandler('log2.txt')
+handler1 = logging.FileHandler('log1.log')
+handler2 = logging.FileHandler('log2.log')
 
 # Create formatters for each handler
 formatter1 = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -24,7 +24,8 @@ handler2.setFormatter(formatter2)
 logger1.addHandler(handler1)
 logger2.addHandler(handler2)
 
+test = 'pp'
 # Log some messages
 logger1.debug('This is a debug message for log1')
 logger1.info('This is an info message for log1')
-logger2.info('This is an info message for log2')
+logger2.info(f'This is an  {test} info message for log2')
